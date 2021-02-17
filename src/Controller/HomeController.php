dbbfa3d436063c165bc1f2852c4e92a74b6ff020
PEREZ Karla <karla.perez@enedis.fr>
2021-02-17 11:00:04 +0100

    last week changes

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -18,4 +18,23 @@ class HomeController extends AbstractController
         return $this->render('home/index.html.twig');
     }
 
+    /**
+     * @Route("/export", name="export")
+     * @return Response
+     */
+    public function export(RoomRepository $repository)
+    {
+        $fp = fopen('php://temp', 'w');
+        fputcsv($fp, array('id', 'name', 'capacity', 'category'));
+        $rooms = $repository->findAll();
+        foreach ($rooms as $room) {
+            fputcsv($fp, array($room->getId(), $room->getName(), $room->getCapacity(), $room->getCategory()->getName()));
+        }
+        rewind($fp);
+        $response = new Response(stream_get_contents($fp));
+        fclose($fp);
+        $response->headers->set('Content-Type', 'text/csv');
+        $response->headers->set('Content-Disposition', 'attachment; filename="testing.csv"');
+        return $response;
+    }
 }
